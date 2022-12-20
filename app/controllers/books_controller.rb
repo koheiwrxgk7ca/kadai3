0338c20643a88957b514a7e588b788f00807174e
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2022-12-20 13:50:06 +0000

    [Continue]kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,14 +8,17 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @booknew = Book.new
+    @book = Book.find(params[:id])
   end
 end
 
